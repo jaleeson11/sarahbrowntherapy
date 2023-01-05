1af4e3a527d77aaa88ab92580c7ae2cdee71c0a5
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2023-01-05 13:39:30 +0000

    Implemented contact banner into footer

--- footer.php
+++ footer.php
@@ -12,6 +12,42 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<div class="site-contact-banner">
+			<div class="container">
+				<div class="site-contact-banner__inner">
+					<div class="site-contact-banner__location">
+						<h4 class="site-contact-banner__heading">
+							<?php echo esc_html( get_theme_mod( 'contact_banner_location' ) ); ?>
+						</h4>
+						<p class="site-contact-banner__sub-heading">
+							<?php echo esc_html( get_theme_mod( 'contact_banner_location_sub-heading' ) ); ?>
+						</p>
+					</div><!-- .site-contact-banner__location -->
+					<div class="site-contact-banner__email">
+						<a href="mailto: <?php echo esc_url( get_option( 'admin_email' ) ); ?>">
+							<h4 class="site-contact-banner__heading">
+								<?php echo esc_html( get_theme_mod( 'contact_banner_email' ) ); ?>
+							</h4>
+							<p class="site-contact-banner__sub-heading">
+								<?php echo esc_html( get_theme_mod( 'contact_banner_email_sub-heading' ) ); ?>
+							</p>
+						</a>
+					</div><!-- .site-contact-banner__email -->
+					<div class="site-contact-banner__phone">
+						<?php $phone_num = get_theme_mod( 'contact_banner_phone' ); ?>
+						<a href="tel: <?php echo esc_url( $phone_num ); ?>">
+							<h4 class="site-contact-banner__heading">
+								<?php echo esc_html( $phone_num ); ?>
+							</h4>
+							<p class="site-contact-banner__sub-heading">
+								<?php echo esc_html( get_theme_mod( 'contact_banner_phone_sub-heading' ) ); ?>
+							</p>
+						</a>
+					</div><!-- .site-contact-banner__phone -->
+				</div><!-- .site-contact-banner__inner -->
+			</div><!-- .container -->
+		</div><!-- .site-contact-banner -->
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sarahbrowntherapy' ) ); ?>">
 				<?php
