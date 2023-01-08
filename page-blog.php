87d408f0d08919611ed89ccc2a7777fc66899e0c
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2023-01-08 16:32:43 +0000

    Added template for blog page

---
+++ page-blog.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The template for displaying the blog page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package sarahbrowntherapy
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+		endwhile; // End of the loop.
+
+		$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+		$posts = new WP_Query(
+			array(
+				'post_type'      => 'post',
+				'paged'          => $paged,
+				'posts_per_page' => 5,     
+			)
+		);
+
+		?>
+		<section class="site-section">
+
+			<div class="container">
+				<?php
+				if ( $posts->have_posts() ) {
+					while ( $posts->have_posts() ) {
+						$posts->the_post();
+						
+						get_template_part( 'template-parts/content', 'list-item' );
+					}
+
+					$total_pages = $posts->max_num_pages;
+					if ( $total_pages > 1 ) :
+						$current_page = max( 1, get_query_var( 'paged' ) );
+						?>
+						<div class="pagination-links">
+							<?php
+							echo wp_kses_post( 
+								paginate_links(
+									array(
+										'current'      => $current_page,
+										'total'        => $total_pages,
+										'prev_text'    => 'prev',
+										'next_text'    => 'next',
+									)
+								) 
+							)
+							?>
+						</div><!-- .pagination-links -->
+						<?php
+					endif;
+				} else {
+					?>
+					<h2><?php echo esc_html( 'Looks like there\'s no posts to display...' ); ?></h2>
+					<?php
+				}
+
+				wp_reset_postdata();
+				?>
+
+			</div><!-- .container -->
+
+		</section><!-- .site-section -->
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
