9354a69af189b4ede9750640917f4335c25afbdb
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2023-01-09 13:54:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -379,7 +379,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkunderscores"] = self["webpackChunkunderscores"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunksarahbrowntherapy"] = self["webpackChunksarahbrowntherapy"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	}();
