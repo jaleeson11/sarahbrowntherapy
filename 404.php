f8ba1ea6814bb7acfaa266d3e9fe64c51299131c
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2023-01-09 09:46:40 +0000

    Removed widgets and search from 404 template

--- 404.php
+++ 404.php
@@ -12,47 +12,19 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'sarahbrowntherapy' ); ?></h1>
-			</header><!-- .page-header -->
+		<div class="container">
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'sarahbrowntherapy' ); ?></p>
+			<section class="error-404 not-found site-section">
+				<header class="page-header">
+					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'sarahbrowntherapy' ); ?></h1>
+				</header><!-- .page-header -->
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'sarahbrowntherapy' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$sarahbrowntherapy_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'sarahbrowntherapy' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$sarahbrowntherapy_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+				<div class="page-content">
+					<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'sarahbrowntherapy' ); ?></p>
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+			
+		</div><!-- .container -->
 
 	</main><!-- #main -->
 
