5ecd7ecbce592ef626a5552bdd5b5f2c95575178
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2023-01-11 09:39:25 +0000

    Changed menu toggle element to div

--- header.php
+++ header.php
@@ -34,11 +34,11 @@
 					?>
 				</div><!-- .site-branding -->
 				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<div class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
 						<span></span>
 						<span></span>
 						<span></span>
-					</button>
+					</div>
 					<?php
 					wp_nav_menu(
 						array(
