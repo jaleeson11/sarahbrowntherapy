6a6060eefaeaac42d5d3b1983fcd7b73ce037fed
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2023-01-13 12:25:13 +0000

    Added meta description

--- header.php
+++ header.php
@@ -15,6 +15,11 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="<?php if ( is_single() ) {
+		single_post_title( '', true );
+	} else {
+		bloginfo( 'name' ); echo " - "; bloginfo( 'description' );
+	} ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
